5bc6c97e7241e0abfe533b3954ab100953820a4c
Maximilian Pohl <maximilian@tweedegolf.com>
2023-04-25 09:13:46 +0200

    Upgrade Terminal SDK

---
+++ pos_terminal/src/main/java/module-info.java
@@ -0,0 +1,3 @@
+module nl.ru.sec_protocol.group5 {
+    requires java.smartcardio;
+}
--- pos_terminal/src/main/java/nl/ru/sec_protocol/group5/Terminal.java
+++ pos_terminal/src/main/java/nl/ru/sec_protocol/group5/Terminal.java
@@ -3,8 +3,9 @@ package nl.ru.sec_protocol.group5;
 import javax.smartcardio.*;
 public class Terminal {
     public static void main(String[] args) throws CardException {
-        CardTerminals terminals = TerminalFactory.getDefault().terminals();
-        java.util.List<CardTerminal> terminal_list = terminals.list();
+        var terminals = TerminalFactory.getDefault().terminals();
+        var terminal_list = terminals.list();
+        System.out.println(terminal_list.isEmpty());
         CardTerminal terminal = terminal_list.get(0);
         Card card = terminal.connect("*");
         System.out.println(card.getATR());
