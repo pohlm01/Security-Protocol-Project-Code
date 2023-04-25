e02871480ce2fc24ee5bd79cfd23d9f8a41072ff
Maximilian Pohl <maximilian@tweedegolf.com>
2023-04-25 15:10:33 +0200

    Make terminal communication work

--- pos_terminal/src/main/java/nl/ru/sec_protocol/group5/Terminal.java
+++ pos_terminal/src/main/java/nl/ru/sec_protocol/group5/Terminal.java
@@ -1,11 +1,17 @@
 package nl.ru.sec_protocol.group5;
 
 import javax.smartcardio.*;
+import jnasmartcardio.Smartcardio;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.Security;
+
 public class Terminal {
-    public static void main(String[] args) throws CardException {
-        var terminals = TerminalFactory.getDefault().terminals();
-        var terminal_list = terminals.list();
-        System.out.println(terminal_list.isEmpty());
+    public static void main(String[] args) throws CardException, NoSuchAlgorithmException, NoSuchProviderException {
+        Security.addProvider(new Smartcardio());
+        CardTerminals terminals = TerminalFactory.getInstance("PC/SC", null, Smartcardio.PROVIDER_NAME).terminals();
+
+        java.util.List<CardTerminal> terminal_list = terminals.list();
         CardTerminal terminal = terminal_list.get(0);
         Card card = terminal.connect("*");
         System.out.println(card.getATR());
